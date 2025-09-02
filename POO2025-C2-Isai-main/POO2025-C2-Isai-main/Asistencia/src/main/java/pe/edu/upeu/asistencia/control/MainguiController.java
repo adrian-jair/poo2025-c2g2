5e2ac8b3c2e7b714b5e42f091e0bfca3a6d8dfb9
adrian-jair <adrian08jair@gmail.com>
2025-09-02 12:56:22 -0500

    realisado

--- POO2025-C2-Isai-main/POO2025-C2-Isai-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ POO2025-C2-Isai-main/POO2025-C2-Isai-main/Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -1,13 +1,18 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TabPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
-import pe.edu.upeu.asistencia.repositorio.ParticipanteRepositorio;
 
-import javax.swing.plaf.MenuBarUI;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class MainguiController {
@@ -16,9 +21,66 @@ public class MainguiController {
     private BorderPane bp;
 
     @FXML
-    MenuBar menuBar ;
+    MenuBar menuBar;
     @FXML
     TabPane tabPane;
+    @FXML
+    MenuItem menuItem1, menuItemC;
+    @Autowired
+    ApplicationContext context ;
 
+    @FXML
+    public void initialize() {
+        MenuItemListener mIL=new MenuItemListener();
+        menuItem1.setOnAction(mIL::handle);
+        menuItemC.setOnAction(mIL::handle);
+
+    }
+
+    class MenuItemListener {
+        Map<String, String[]> menuConfig= Map.of(
+                "menuItem1",new String[]{"/fxml/main_participante.fxml","Reg.Participante","T"},
+                "menuItemC",new String[]{"/fxml/login,fxml","salir","C"}
+        );
+        public void handle(ActionEvent e) {
+            String id= ((MenuItem)e.getSource()).getId();
+            if (menuConfig.containsKey(id)) {
+                String[] items = menuConfig.get(id);
+                if (items[2].equals("C")){
+                    Platform.exit();
+                    System.exit(0);
+
+                }else {
+                    abrirTabPaneFXML(items[0],items[1]);
+                }
+
+            }
+
+        }
+        private void abrirTabPaneFXML(String fxmlpath, String tittle){
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlpath));
+                fxmlLoader.setControllerFactory(context::getBean);
+                Parent root = fxmlLoader.load();
+                ScrollPane scrollPane= new ScrollPane(root);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                Tab newTab =new Tab(tittle,scrollPane);
+                tabPane.getTabs().clear();
+                tabPane.getTabs().add(newTab);
+
+
+            }catch (IOException ex){
+                throw new RuntimeException(ex);
+
+            }
+        }
+
+    }
+
+    class MenuListerner {
+
+
+    }
 
 }
