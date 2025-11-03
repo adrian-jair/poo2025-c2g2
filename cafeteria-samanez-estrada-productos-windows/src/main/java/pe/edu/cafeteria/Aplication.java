1c5020f5038551eaf0f80e9c742adb2843b76420
adrian-jair <adrian08jair@gmail.com>
2025-11-03 16:51:48 -0500

    producto

---
+++ cafeteria-samanez-estrada-productos-windows/src/main/java/pe/edu/cafeteria/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.cafeteria;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("holas");
+        ProductosApplication.main(args);
+    }
+}
